81c12c89e93e1d2b0562bd78225de9629c4c0ca3
Vadym Holoveichuk <goloveychuk1@gmail.com>
2018-05-07 15:37:27 +0300

    asd

--- install.bash
+++ install.bash
@@ -1,6 +1,6 @@
 
 # didnt test
-sudo ln -s ./*.service /etc/systemd/system/
-sudo systemctl reload
+sudo cp -f ./homebridge.service /etc/systemd/system/
+sudo systemctl daemon-reload
 sudo systemctl enable homebridge.service
 sudo systemctl restart homebridge.service
